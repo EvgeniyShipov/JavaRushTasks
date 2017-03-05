55e9842c5facc06e4e31bfbffe390679b89e80bb
Shipov <shipov.foto@mail.ru>
2017-03-05 15:59:58 +0300

    add task 2803-2805 from JavaMultithreading

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2805/MyThread.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task28.task2805;
+
+public class MyThread extends Thread {
+    private static int i = 0;
+
+    {
+        int maxPriority = this.getThreadGroup().getMaxPriority();
+        int priority = (i++ % 10) + 1;
+        this.setPriority(priority > maxPriority ? maxPriority : priority);
+    }
+
+    public MyThread() {
+
+    }
+
+    public MyThread(Runnable target) {
+        super(target);
+    }
+
+    public MyThread(ThreadGroup group, Runnable target) {
+        super(group, target);
+    }
+
+    public MyThread(String name) {
+        super(name);
+    }
+
+    public MyThread(ThreadGroup group, String name) {
+        super(group, name);
+    }
+
+    public MyThread(Runnable target, String name) {
+        super(target, name);
+    }
+
+    public MyThread(ThreadGroup group, Runnable target, String name) {
+        super(group, target, name);
+    }
+
+    public MyThread(ThreadGroup group, Runnable target, String name, long stackSize) {
+        super(group, target, name, stackSize);
+    }
+}
