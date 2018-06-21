b5b4fd2971b93dca5efec303f7d484f4b832a9ec
Shipov <shipov.foto@mail.ru>
2018-06-21 22:04:56 +0300

    add task 4004 from JavaCollections

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4004/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task40.task4004;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Принадлежность точки многоугольнику
+*/
+
+class Point {
+    public int x;
+    public int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        List<Point> polygon = new ArrayList<>();
+        polygon.add(new Point(0, 0));
+        polygon.add(new Point(0, 10));
+        polygon.add(new Point(10, 10));
+        polygon.add(new Point(10, 0));
+
+        System.out.println(isPointInPolygon(new Point(5, 5), polygon));
+        System.out.println(isPointInPolygon(new Point(100, 100), polygon));
+    }
+
+    public static boolean isPointInPolygon(Point point, List<Point> polygon) {
+        int[] xpoints = polygon.stream().map(p -> p.x).mapToInt(Integer::intValue).toArray();
+        int[] ypoints = polygon.stream().map(p -> p.y).mapToInt(Integer::intValue).toArray();
+        Polygon polyg = new Polygon(xpoints, ypoints, polygon.size());
+
+        return polyg.contains(new java.awt.Point(point.x, point.y));
+    }
+}
+
