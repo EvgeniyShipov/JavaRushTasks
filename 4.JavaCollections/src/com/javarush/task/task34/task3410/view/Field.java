80f952bc07fe911d5c015c28be95410d0ac236ca
Shipov <shipov.foto@mail.ru>
2018-07-29 18:24:41 +0300

    big task 3410 (14) from JavaCollections

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -1,10 +1,13 @@
 package com.javarush.task.task34.task3410.view;
 
 import com.javarush.task.task34.task3410.controller.EventListener;
+import com.javarush.task.task34.task3410.model.Direction;
 import com.javarush.task.task34.task3410.model.GameObjects;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Field extends JPanel {
 
@@ -13,6 +16,9 @@ public class Field extends JPanel {
 
     public Field(View view) {
         this.view = view;
+        KeyHandler keyHandler = new KeyHandler();
+        addKeyListener(keyHandler);
+        setFocusable(true);
     }
 
     @Override
@@ -26,5 +32,28 @@ public class Field extends JPanel {
 
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
+    }
+
+    public class KeyHandler extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    eventListener.move(Direction.LEFT);
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    eventListener.move(Direction.RIGHT);
+                    break;
+                case KeyEvent.VK_UP:
+                    eventListener.move(Direction.UP);
+                    break;
+                case KeyEvent.VK_DOWN:
+                    eventListener.move(Direction.DOWN);
+                    break;
+                case KeyEvent.VK_R:
+                    eventListener.restart();
+                    break;
+            }
+        }
     }
 }
