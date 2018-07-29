b43b6686807fab00bf1f7cf60445a580a9c045fc
Shipov <shipov.foto@mail.ru>
2018-07-29 18:15:46 +0300

    big task 3410 (13) from JavaCollections

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/controller/Controller.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/controller/Controller.java
@@ -14,6 +14,8 @@ public class Controller implements EventListener {
         view = new View(this);
         view.init();
         model.restart();
+        view.setEventListener(this);
+        model.setEventListener(this);
     }
 
     public static void main(String[] args) {
@@ -22,22 +24,25 @@ public class Controller implements EventListener {
 
     @Override
     public void move(Direction direction) {
-
+        model.move(direction);
+        view.update();
     }
 
     @Override
     public void restart() {
-
+        model.restart();
+        view.update();
     }
 
     @Override
     public void startNextLevel() {
-
+        model.startNextLevel();
+        view.update();
     }
 
     @Override
     public void levelCompleted(int level) {
-
+        view.completed(level);
     }
 
     public GameObjects getGameObjects() {
--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -30,4 +30,7 @@ public class Model {
     public void startNextLevel() {
         restartLevel(++currentLevel);
     }
+
+    public void move(Direction direction) {
+    }
 }
