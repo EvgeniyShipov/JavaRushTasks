67e471b8446f6210954aefd4ec503786cb7824e0
Shipov <shipov.foto@mail.ru>
2018-08-07 23:28:04 +0300

    big task 3410 (15) from JavaCollections

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -32,5 +32,58 @@ public class Model {
     }
 
     public void move(Direction direction) {
+        Player player = gameObjects.getPlayer();
+        if (checkWallCollision(player, direction) || checkBoxCollisionAndMoveIfAvaliable(direction))
+            return;
+
+        moveObject(player, direction);
+        checkCompletion();
+    }
+
+    public boolean checkWallCollision(CollisionObject gameObject, Direction direction) {
+        return gameObjects.getWalls().stream()
+                .anyMatch(wall -> gameObject.isCollision(wall, direction));
+    }
+
+    public boolean checkBoxCollisionAndMoveIfAvaliable(Direction direction) {
+        for (Box box : gameObjects.getBoxes()) {
+            if (gameObjects.getPlayer().isCollision(box, direction)) {
+                for (Box anotherBox : gameObjects.getBoxes()) {
+                    if (box.isCollision(anotherBox, direction) || checkWallCollision(box, direction))
+                        return true;
+                }
+                moveObject(box, direction);
+            }
+        }
+        return false;
+    }
+
+    private void moveObject(Movable object, Direction direction) {
+        switch (direction) {
+            case UP:
+                object.move(0, -FIELD_CELL_SIZE);
+                break;
+            case DOWN:
+                object.move(0, FIELD_CELL_SIZE);
+                break;
+            case LEFT:
+                object.move(-FIELD_CELL_SIZE, 0);
+                break;
+            case RIGHT:
+                object.move(FIELD_CELL_SIZE, 0);
+                break;
+        }
+    }
+
+    public void checkCompletion() {
+        long boxes = gameObjects.getBoxes().stream()
+                .mapToLong(box -> gameObjects.getHomes().stream()
+                        .filter(home -> box.getX() == home.getX() && box.getY() == home.getY())
+                        .count())
+                .sum();
+
+        if (boxes == gameObjects.getBoxes().size()) {
+            eventListener.levelCompleted(currentLevel);
+        }
     }
 }
